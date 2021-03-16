a52ae86a372f8b72887068d44278766398038c0f
Ian Manor <imanor@apple.com>
2021-03-15 23:16:16 -0300

    Super senha parte 1

---
+++ iOSBasico/Xcode12/SuperSenha/SuperSenha/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SuperSenha
+//
+//  Created by Ian Manor on 15/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        // play na animacao
+    }
+
+
+}
+
